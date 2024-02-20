46f3fc4d9c23cd957cbae6a483b9489b82d24213
Radoclav79 <mladenovicradoclav@gmail.com>
2024-02-20 10:38:43 +0100

    create with content of 4-puts.c file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,9 @@
 #include<stdio.h>
 
-int puts(const char * "Programming is like building a multilingual puzzle")
+/* Text prints */
+
+int main(void)
+{
+puts ("\"Programming is like building a multilingual puzzle");
+return (0);
+}
