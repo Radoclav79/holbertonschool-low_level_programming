defca708fafacfbdaf8a2e8d4775587d23927305
Radoclav79 <mladenovicradoclav@gmail.com>
2024-02-21 10:22:42 +0100

    7th content of 0-positive_or_negative.c file

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,10 +10,10 @@ int main(void)
 	if (n > 0)
 		printf("%d is positve\n", n);
 		
-	else if (n == 0)
-		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
 	
 	else
-		printf("%d is negative\n", n);
+		printf("%d is zero\n", n);
 	return (0);
 }
