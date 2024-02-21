47f9283800f86b1436c4aeff3e43aeaacbff2ec3
Radoclav79 <mladenovicradoclav@gmail.com>
2024-02-21 10:21:14 +0100

    6th content of 0-positive_or_negative.c file

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there
- * betty style doc for function main goes there
- */
+
 int main(void)
 {
 	int n;
@@ -10,16 +8,12 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-	{
-		printf("is positve\n");
-	}
+		printf("%d is positve\n", n);
+		
 	else if (n == 0)
-	{
-		printf("is zero\n");
-	}
+		printf("%d is zero\n", n);
+	
 	else
-	{
-		printf("is negative\n");
-	}
+		printf("%d is negative\n", n);
 	return (0);
 }
