846d1f19278926ad54cad8da7cd7464f01f52f14
Radoclav79 <mladenovicradoclav@gmail.com>
2024-02-21 16:22:56 +0100

    create with content of 6-print_numberz.c file

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints numberz
+ *
+ * Return: always 0 success
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
