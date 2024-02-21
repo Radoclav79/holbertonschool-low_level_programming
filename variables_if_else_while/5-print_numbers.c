090b1daf8424791f21adeef5f5b9ca9409e0ba27
Radoclav79 <mladenovicradoclav@gmail.com>
2024-02-21 16:10:55 +0100

    create with content of 5-print_numbers.c file

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print numbers
+ *
+ * Return: always 0 success
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	return (0);
+}
