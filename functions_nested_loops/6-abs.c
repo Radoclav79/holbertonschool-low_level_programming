fb48075297e96a4c509f6f20b7d03080a5f8cb6e
Radoclav79 <mladenovicradoclav@gmail.com>
2024-02-23 15:47:09 +0100

    create with content of 6-abs.c and 6-main.c files

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - compute absolute value
+ * @n: check value
+ *
+ * Return: abs value
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
