1a34433ab6b2d163355f3093ecbd28cf8940935f
Radoclav79 <mladenovicradoclav@gmail.com>
2024-02-23 10:48:51 +0100

    commit with all function to all files and block

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alahabet - print lowercase alphabet
+ *
+ * Return: always 0 success
+ */
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		_putchar(i);
+	}
+}
