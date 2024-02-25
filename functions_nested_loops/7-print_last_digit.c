6b616f3c1a7d12465ad4efad72163f99ab872c99
Radoclav79 <mladenovicradoclav@gmail.com>
2024-02-25 15:07:06 +0100

    create with content of 7-main.c and 7-print_last_digit.c files

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit
+ * @c: number of printed last digit
+ *
+ * Return: value of last digit
+ */
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
