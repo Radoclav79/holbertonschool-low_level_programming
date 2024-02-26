9f4cf4e91d341cbd3f32137129643c04b629cac2
Radoclav79 <mladenovicradoclav@gmail.com>
2024-02-26 20:28:08 +0100

    nth content of 5-more_numbers.c file

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,8 +12,15 @@ void more_numbers(void)
 	{
 		for (j = 0 ; j <= 14 ; j++)
 		{
-			_putchar (j / 10 + '0');
-			_putchar ('0' + j % 10);
+			if (j < 10)
+			{
+				_putchar ('0' + j % 10);
+			}
+			else
+			{
+				_putchar (j / 10 + '0');
+				_putchar ('0' + j % 10);
+			}
 		}
 		_putchar ('\n');
 	}
