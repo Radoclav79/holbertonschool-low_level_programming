4aaa26339cf5bec28c98b79c08bb1e2a2b5ee223
Radoclav79 <mladenovicradoclav@gmail.com>
2024-02-26 21:48:03 +0100

    create with contentof 6-main.c and 6-print_line.c files

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line - print line
+ * @n: parameter
+ *
+ * Return: line
+ */
+void print_line(int n)
+{
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0 ; i <= n ; i++)
+		{
+			_putchar('_');
+		}
+	}
+}
