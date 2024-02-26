db4a2c77d2f69b9b5eafab087a8c80770b1cf4b5
Radoclav79 <mladenovicradoclav@gmail.com>
2024-02-26 11:49:56 +0100

    create with content of 2-main.c and 2-mul.c files

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiple two parameters numbers
+ * @a: first number to mulptiplie
+ * @b: second parameter of multiple
+ *
+ * Return: value of multiple
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
