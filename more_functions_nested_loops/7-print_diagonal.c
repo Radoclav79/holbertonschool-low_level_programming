abf583661a01defe2cc9bb20cb829463aa0d2b8b
Radoclav79 <mladenovicradoclav@gmail.com>
2024-02-27 14:59:26 +0100

    create with content of 7-main.C and 7-print_diagonal.c files

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagonal
+ * @n: number of time caracter
+ *
+ * Return: diagonal
+ */
+void print_diagonal(int n)
+{
+	int j, k;
+	
+	if (n > 0)
+	{
+		for (j = 0 ; j < n ; j++)
+		{
+			for (k = 0 ; k < j ; k++)
+			{
+				_putchar(' ');
+			}
+		_putchar('\\');
+		_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
