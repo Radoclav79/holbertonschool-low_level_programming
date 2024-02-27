4cbb7e0dbd5c1a497d425c7cf9c8cdc812848dac
Radoclav79 <mladenovicradoclav@gmail.com>
2024-02-27 16:03:19 +0100

    create with content of 8-main.c and 8-print_square.c files

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - print sqaure
+ * @size: size of square
+ *
+ * Return: n * squre + n * line
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		if (size == 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			for (j = 0 ; j < size ; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
