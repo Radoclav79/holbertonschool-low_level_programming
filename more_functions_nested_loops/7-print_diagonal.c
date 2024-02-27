5273ab11356c0ba2c0b71c6794b5f20aebf39d4a
Radoclav79 <mladenovicradoclav@gmail.com>
2024-02-27 15:26:12 +0100

    3rd content of 7-print_diagonal. file

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,14 +8,16 @@
 void print_diagonal(int n)
 {
 	int j, k;
-	
 	if (n > 0)
 	{
 		for (j = 0 ; j < n ; j++)
 		{
 			for (k = 0 ; k <= j ; k++)
 			{
-				_putchar(' ');
+				if (k != 0 || k != 1)
+				{
+					_putchar(' ');
+				}
 			}
 		_putchar('\\');
 		_putchar('\n');
