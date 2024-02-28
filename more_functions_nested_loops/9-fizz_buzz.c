59161dd62fb000de4653d5222a473d47c78894eb
Radoclav79 <mladenovicradoclav@gmail.com>
2024-02-28 09:58:44 +0100

    create with content of 9-fizzbuzz.c file

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - f print 1 to 100
+ *
+ * Return: 1 to 100, "fizz"if  3,"buzz" if 5 and "FizzBuzz" it is 3 and 5
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 100 ; i++)
+	{
+		if ((i + 1) % 3 && (i + 1) % 5)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((i + 1) % 3)
+		{
+			printf("Fizz ");
+		}
+		else if ((i + 1) % 5)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i + 1);
+		}
+	}
+	return (0);
+}
