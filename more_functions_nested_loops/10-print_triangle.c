34aeee7b8f8cbf9e83e4f54ab18dbeafef440c21
Radoclav79 <mladenovicradoclav@gmail.com>
2024-02-28 11:26:07 +0100

    create with content of 10-main.c and 10-print_triangle.c files

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_triangle - print traingle
+ * @size: lenght parameter
+ *
+ * Return: triangle
+ */
+void print_triangle(int size)
+{
+	int i, j, k, l;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		for (j = 0 ; j < size ; j++)
+		{
+			for (k = 0 ; k < (size - 2) ; k++)
+			{
+					_putchar(' ');
+			}
+			for (l = 0 ; l < (size - k) ; l++)
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+}
